42daa0c065d6f507ce610382882b43120f6cb798
ACash01 <125855363+ACash01@users.noreply.github.com>
2023-02-22 10:20:43 +0800

    Add files via upload

---
+++ js/scriptUse.js
@@ -0,0 +1,18 @@
+const logo = document.querySelector(".logo");
+const header = document.querySelector(".Hnav")
+window.addEventListener("scroll",changeHead);
+let iniView = 0
+
+function changeHead() {
+    var viewing = window.scrollY;
+
+    if(viewing < iniView) {
+        logo.classList.replace("logo-center","logo-left")
+        header.classList.replace("nav-hide","nav-show")
+    } else {
+        logo.classList.replace("logo-left","logo-center")
+        header.classList.replace("nav-show","nav-hide")
+    }
+
+    iniView = window.scrollY
+}
